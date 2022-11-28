2779d6a6f9300ead8f57e31742f8a3970d0c5354
amos <amosjshinkle@gmail.com>
2022-11-28 10:43:50 -0500

    commit tree b625ac124e9a48be6ea0fd8559c5d4d0be879a39

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42
+    const distance = someValue - hqLocation; 
+    if (distance < 0) {
+        return Math.abs(distance); 
+    }
+    else {
+        return distance
+    }
+  };
+
+  function distanceFromHqInFeet(someValue) {
+    return (distanceFromHqInBlocks(someValue) * 264);
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const travelDistance = start - destination; 
+    if (travelDistance < 0) {
+        return Math.abs (travelDistance) * 264; 
+    }
+    else {
+        return travelDistance * 264;
+    }
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const cabDistance = distanceTravelledInFeet(start, destination);
+    if (cabDistance < 400){
+        return 0;
+    } else if (cabDistance >= 400 && cabDistance <= 2000){
+        return (cabDistance - 400) * 0.02;
+    } else if (cabDistance >= 2000 && cabDistance <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+}
